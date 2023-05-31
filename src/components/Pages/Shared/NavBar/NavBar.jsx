239c8c711415074f6375f413e84921ac2fd3d7e5
coders rabbi <pcrabbi2020@gmail.com>
2023-05-31 14:04:57 +0600

    Carousel add, navbar fixed

--- src/components/Pages/Shared/NavBar/NavBar.jsx
+++ src/components/Pages/Shared/NavBar/NavBar.jsx
@@ -10,7 +10,7 @@ const NavBar = () => {
     </>
     return (
         <div>
-            <div className="navbar bg-base-100">
+            <div className="navbar fixed z-10 bg-black bg-opacity-30 max-w-screen-xl text-white">
                 <div className="navbar-start">
                     <div className="dropdown">
                         <label tabIndex={0} className="btn btn-ghost lg:hidden">
